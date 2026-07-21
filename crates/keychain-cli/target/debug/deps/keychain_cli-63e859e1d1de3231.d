2500ba9d2c4d255c77fa3c9d9f0c3d779f6b30a9
zeonzip <96481337+zeonzip@users.noreply.github.com>
2026-07-21 22:24:35 +0200

    gitignore is taking the piss

--- crates/keychain-cli/target/debug/deps/keychain_cli-63e859e1d1de3231.d
+++
@@ -1,5 +0,0 @@
-/Users/zeon/Desktop/Projects/Rust/keychain/crates/keychain-cli/target/debug/deps/keychain_cli-63e859e1d1de3231.d: src/main.rs
-
-/Users/zeon/Desktop/Projects/Rust/keychain/crates/keychain-cli/target/debug/deps/libkeychain_cli-63e859e1d1de3231.rmeta: src/main.rs
-
-src/main.rs:
--- crates/keychain-cli/target/debug/deps/keychain_cli-cae03003aa94cb99.d
+++
@@ -1,5 +0,0 @@
-/Users/zeon/Desktop/Projects/Rust/keychain/crates/keychain-cli/target/debug/deps/keychain_cli-cae03003aa94cb99.d: src/main.rs
-
-/Users/zeon/Desktop/Projects/Rust/keychain/crates/keychain-cli/target/debug/deps/libkeychain_cli-cae03003aa94cb99.rmeta: src/main.rs
-
-src/main.rs:
